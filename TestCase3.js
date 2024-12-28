b8388bc045c9088b7935314963cc8e76b3608988
Barep Jati K <barepperab@gmail.com>
2024-12-28 15:09:12 +0700

    Page Object Model task

--- TestCase3.js
+++ TestCase3.js
@@ -1,6 +1,6 @@
 const {Builder}=require('selenium-webdriver');
 const LoginPage=require('./WebComponent/LoginPage');
-// const DashboardPage=require('./WebComponent/DashboardPage');
+const DashboardPage=require('./WebComponent/DashboardPage');
 const CartPage=require('./WebComponent/CartPage');
 const CartBadgePage=require('./WebComponent/CartBadgePage');
 const assert=require('assert');
@@ -19,18 +19,15 @@ describe('Test Case 3', function(){
         await loginPage.login('standard_user', 'secret_sauce');
     });
 
-    // it('Login successfully and verify dashboard', async function(){
-    //     const dashboardPage=new DashboardPage(driver);
-    //     const title=await dashboardPage.isOnDashboard();
-    //     assert.strictEqual(title, 'Products', 'Expected dashboard title to be Products');
-    // });
-
-    beforeEach(async function(){
-        const cartPage=new CartPage(driver);
-        await cartPage.addToCart();
+    it('Login successfully and verify dashboard', async function(){
+        const dashboardPage=new DashboardPage(driver);
+        const title=await dashboardPage.isOnDashboard();
+        assert.strictEqual(title, 'Products', 'Expected dashboard title to be Products');
     });
 
     it('Added to cart successfully and verify cart', async function(){
+        const cartPage=new CartPage(driver);
+        await cartPage.addToCart();
         const cartbadge=new CartBadgePage(driver);
         const cartBadge=await cartbadge.itemAdded();
         assert.strictEqual(cartBadge, '2', 'Expected shopping cart badge appear with number equal item added');
